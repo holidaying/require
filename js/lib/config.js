315fb97cc92fe695211c1786a91f7fcc5ff7b244
库克 <workliudan@163.com>
2016-07-07 14:04:12 +0800

    config的js的全局变量可以直接引用不需要有依赖

---
+++ js/lib/config.js
@@ -0,0 +1,22 @@
+var report_url = 'http://yaotv.holdfun.cn/reportlog/';
+// var domain_url = 'https://test.holdfun.cn/portal/';
+// var business_url = 'http://test.holdfun.cn/LeEdu/mobile/';
+// var mpappid = 'wxc5d930ea846a40e1';
+// var busiAppId = 'wxb4196cbe04d66ab6';
+// var eduUuid = "d002ab37b956432790715b922ea80d46";
+// 乐宁
+var mpappid = 'wxae5fda78c0bcab03';
+var busiAppId = 'wxae5fda78c0bcab03';
+
+
+// 正式
+var domain_url = "http://hummerclan.com";
+var eduUuid = "7cedeef99ac54096b862aa25cebda90a";
+var business_url = "http://hummerclan.com/LeEdu/mobile/";
+
+var shareData = {
+	'imgUrl': share_img,
+	'link': share_url,
+	'desc': share_desc,
+	'title': share_title
+};
